dbdc133b56f7f692ceb6a32c233a32c599642f9e
Ronan <rsf.sho@gmail.com>
2018-09-16 20:44:03 -0700

    app skeleton

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="page home">
+    <div class="home__send"></div>
+    <div class="home__dash"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  
 }
 </script>
