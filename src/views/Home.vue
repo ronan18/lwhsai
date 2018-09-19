665dbb18455d0a3ad880ebf9358dae44805d5f94
Ronan <rsf.sho@gmail.com>
2018-09-18 22:48:22 -0700

    new ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,22 @@
 <template>
-<div class="page home">
-  <div class="home__send">
-    <div class="send__messages">
-      <div class="message">
-        <p class="message__text">You're terrible <span class="message__score">1</span></p>
-        <p class="message__time">0.5s</p>
-      </div>
-
-    </div>
-    <form class="send__form" @submit.prevent>
-      <input type="text" name="message" class="send__form__message"  placeholder="Say something" autocomplete="off">
-      <button class="send__form__submit" type="submit">Send</button>
-    </form>
-  </div>
-  <div class="home__dash">
-    <p class="dash__score yellow">50</p>
-    <div class="dash__slider">
-      <div class="slider__description">
-        <p>positive</p>
-      <p>negative</p>
-      </div>
-      
-      <div class="sliderContainer">
-        <input class="slider" type="range" min="1" max="100" value="50" disabled>
-      </div>
-    </div>
-    <p class="dash__stat">computational time: 0.5s</p>
-    </div>
-  </div>
+<form class="page home" @submit.prevent="submit">
+  <label  for="message">Check the sentiment of a sentence by typing in the box below</label>
+  <input v-model="message" autocomplete="off" type="text" name="message" placeholder="type here">
+  </form>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data() {
+    return {
+      message: ''
+    }
+  },
+  methods: {
+    submit() {
+      this.$router.push({name: 'result', params: { query: this.message}})
+    }
+  }
 }
 </script>
---
+++ src/views/Result.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="page result">
+  <div v-if="!results" class="loading">
+    <div class="loading-bar"></div>
+    <div class="loading-bar"></div>
+    <div class="loading-bar"></div>
+    <div class="loading-bar"></div>
+  </div>
+  <p v-if="!results" class="loading__text">Calculating</p>
+  <p v-if="results" class="result__num" :class="colorClass">{{round(results.sentiment)}}</p>
+  <div @keyup.space="reload()" v-if="results" class="sliderContainer">
+    <div class="slider__label__row">
+      <label class="slider__label">negative</label>
+      <label class="slider__label">positive</label>
+    </div>
+
+    <input v-model="results.sentiment"  type="range" class="slider" disabled>
+  </div>
+    <p v-if="results" class="results__time">computed in {{results.time}}s</p>
+    <router-link to="/" v-if="results" class="btn">Run Again</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+
+      results: null
+    }
+  },
+  mounted() {
+
+    let message = this.$route.params.query
+    setTimeout(() => {
+      this.results = this.getData(message)
+    }, 500)
+    window.addEventListener("keypress", (e) => {
+      if (e.code == "Space" && this.$route.path == '/result') {
+        this.reload()
+      }
+    });
+
+  },
+  methods: {
+    getData(message) {
+      return {
+        sentiment: Math.random() * 100,
+        time: Math.random().toFixed(2)
+      }
+    },
+    round(i) {
+      return Math.round(i)
+    },
+    reload() {
+      this.$router.push('/')
+    }
+  },
+  computed: {
+    colorClass() {
+      return {
+        green: this.results.sentiment >= 66,
+        yellow: this.results.sentiment < 66 && this.results.sentiment > 33,
+        red: this.results.sentiment <= 33
+      }
+    }
+  }
+
+}
+</script>
